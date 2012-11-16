4992ae0c9c682e5648cbf38a0119416f89da1b36
Mike Meier <mike.meier@ibrows.ch>
2012-11-16 22:29:46 +0100

    no message

--- src/mikemeier/DemoPHPNodeBridge/Controller/IndexController.php
+++ src/mikemeier/DemoPHPNodeBridge/Controller/IndexController.php
@@ -2,6 +2,8 @@
 
 namespace mikemeier\DemoPHPNodeBridge\Controller;
 
+use mikemeier\PHPNodeBridge\Service\Bridge;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -19,5 +21,24 @@ class IndexController extends Controller
     public function indexAction()
     {
         return array();
+    }
+
+    /**
+     * @Route("/userlist", name="userlist")
+     * @Template()
+     */
+    public function userlistAction()
+    {
+        return array(
+            'users' => $this->getBridge()->getUserContainer()->getAll()
+        );
+    }
+
+    /**
+     * @return Bridge
+     */
+    protected function getBridge()
+    {
+        return $this->get('mikemeier_php_node_bridge.bridge');
     }
 }
